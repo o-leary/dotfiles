3cd3bc87180ce5e2c26085b8c2aaeb2ba99d8135
Sean O'Leary <sean@o-leary.co.nz>
2026-03-01 18:35:48 +1300

    Fix package installation commands in PowerShell script

--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -1,7 +1,8 @@
 # Required packages (Needed for everything to work)
+wsl --install;
 winget install Git.Git -e -i;
-winget install Microsoft.WindowsTerminal --silent
-winget install --id glzr-io.glazewm --exact --version 3.9.1 --no-upgrade --silent
+winget install Microsoft.WindowsTerminal --silent;
+winget install --id glzr-io.glazewm --exact --version 3.9.1 --no-upgrade --silent;
 # winget install --id glzr-io.zebar --exact --version 3.1.1 --no-upgrade --silent # Zebar now come with glazewm
 #winget install --id eza-community.eza -e --silent;
 winget install --id MikeFarah.yq;
