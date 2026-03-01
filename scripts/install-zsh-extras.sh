7550dbd194bc593774930596bb0539a3f33b9453
Sean O'Leary <sean@o-leary.co.nz>
2026-03-01 19:59:34 +1300

    Add script to install ZSH extras
    This script installs various ZSH themes and plugins.

---
+++ scripts/install-zsh-extras.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Theme: Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/themes/powerlevel10k
+# Install Fast Syntax Highlighting Plugin
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.config/zsh/plugins/fast-syntax-highlighting
+# Install Autosuggestions Plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+# Install History Substring Search Plugin
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh/plugins/zsh-history-substring-search
